1ac4950e17f1233ec7afb3e746c1301a030e4663
charlesquik <charlesquik@hotmail.com>
2013-10-10 18:00:27 -0400

    charles netbeans

---
+++ src/intervensim/Intervensim.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intervensim;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Charles
+ */
+public class Intervensim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+}
