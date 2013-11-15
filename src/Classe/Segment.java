6e51a20555d0fe200f8e2cc5fe45a362b681c66b
josephpatry <jp250293@gmail.com>
2013-11-15 14:45:40 -0500

    Modification de la propriété getDistance pour calculer la distance a chaque appel

--- src/Classe/Segment.java
+++ src/Classe/Segment.java
@@ -15,23 +15,18 @@ public class Segment {
     
     Noeud m_Noeud2;
     
-    double m_Distance;
-    
     //Constructeur
     Segment(Noeud p_Noeud1, Noeud p_Noeud2)
     {
         m_Noeud1 =  p_Noeud1;
         m_Noeud2 = p_Noeud2;
-        
-        //Calcul de la distance du segment avec pythagore
-        m_Distance = Math.pow((double)Math.abs(m_Noeud1.obtenir_posX() - m_Noeud2.obtenir_posX()), 2) + Math.pow((double)Math.abs(m_Noeud1.obtenir_posY() - m_Noeud2.obtenir_posY()), 2);
-        m_Distance = Math.sqrt(m_Distance);
     }
     
     //Retourner la distance
     double getDistance()
     {
-        return m_Distance;
+        double distance = Math.pow((double)Math.abs(m_Noeud1.obtenir_posX() - m_Noeud2.obtenir_posX()), 2) + Math.pow((double)Math.abs(m_Noeud1.obtenir_posY() - m_Noeud2.obtenir_posY()), 2);
+        return Math.sqrt(distance);
     }
     
 }
